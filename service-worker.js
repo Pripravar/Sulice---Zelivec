f27ab237b4ae39bb7ea5d2052bae71e5326848ea
Pripravar <arasid33@gmail.com>
2026-05-14 17:08:00 +0200

    deník

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
    ════════════════════════════════════════════════════════════════ */
 
 // VERZE cache - při změně příště zvedni, ať si telefony stáhnou novou verzi.
-const CACHE_VERSION = 'sulice-v15-2026-05-12';
+const CACHE_VERSION = 'sulice-v16-2026-05-12';
 
 const CORE_ASSETS = [
   './',
