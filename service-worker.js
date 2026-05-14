d199876d8b13370e71c354363e32a1b6177a2528
Pripravar <arasid33@gmail.com>
2026-05-14 17:31:30 +0200

    KZP

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
    ════════════════════════════════════════════════════════════════ */
 
 // VERZE cache - při změně příště zvedni, ať si telefony stáhnou novou verzi.
-const CACHE_VERSION = 'sulice-v17-2026-05-12';
+const CACHE_VERSION = 'sulice-v18-2026-05-12';
 
 const CORE_ASSETS = [
   './',
