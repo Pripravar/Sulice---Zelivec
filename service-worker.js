4be781d13364244c91bffe62df16dc1dd23958bd
Pripravar <arasid33@gmail.com>
2026-05-15 07:50:19 +0200

    fotky

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
    ════════════════════════════════════════════════════════════════ */
 
 // VERZE cache - při změně příště zvedni, ať si telefony stáhnou novou verzi.
-const CACHE_VERSION = 'sulice-v22-2026-05-12';
+const CACHE_VERSION = 'sulice-v23-2026-05-12';
 
 const CORE_ASSETS = [
   './',
