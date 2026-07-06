4b9b4d9db0a1d82d52036d0cedfc38c176e3627a
Ondrej Svoboda <arasid33@gmail.com>
2026-07-06 20:35:11 +0200

    d

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
    ════════════════════════════════════════════════════════════════ */
 
 // VERZE cache - při změně příště zvedni, ať si telefony stáhnou novou verzi.
-const CACHE_VERSION = 'sulice-v88-2026-07-01';
+const CACHE_VERSION = 'sulice-v89-2026-07-06';
 
 const CORE_ASSETS = [
   './',
